a3938f6edbac5a24e2bf0471865d09e0091cd6e5
Thinh M. Le <thinh.m.le@gmail.com>
2017-05-25 07:47:54 -0700

    Initial Commit

---
+++ HeartBeat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HeartBeat
+//
+//  Created by Thinh Le on 2017-05-25.
+//  Copyright © 2017 Lac Viet Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
